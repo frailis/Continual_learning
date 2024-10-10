abf9cd45c2f2539bb7507f650e7efeec9fea4a2e
frailis <d.frailis@studenti.unipi.it>
2024-10-10 16:17:35 +0200

    piccole modifiche

--- src/plotter.py
+++ src/plotter.py
@@ -54,4 +54,4 @@ for i in range(1, len(sys.argv)):
     str = './tensorboard_logs/' + sys.argv[i]
     logs.append(str)
 tag = 'Accuracy'  
-plot_logs(logs, tag, xlabel='Iterations', ylabel='Accuracy', title='Accuracy for the first task M=0.0')
+plot_logs(logs, tag, xlabel='Iterations', ylabel='Accuracy', title='Accuracy for the first task M=0.1')
